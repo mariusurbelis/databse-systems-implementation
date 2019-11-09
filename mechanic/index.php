6786008927b481085c7737de5eb46eca9bc35654
Marius Urbelis <mariusurbelis23@gmail.com>
2019-11-09 00:31:44 +0000

    Beginning of the mechanic screen

---
+++ mechanic/index.php
@@ -0,0 +1,28 @@
+<head>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Mechanic Screen</title>
+    
+    <style>
+    
+        .dev {
+            border: solid 2px green;
+        }
+    
+    </style>
+
+</head>
+
+<body>
+
+    <div class="container-fluid">
+    
+        <div class="row">
+            <div class="dev col-8">ok</div>
+            <div class="dev col-4">ok</div>
+        </div>
+    
+    </div>
+
+</body>
